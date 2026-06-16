238a7e6635b434cf90682e400bcaec5f982f20b1
Emanuele Favero <emafaux89@gmail.com>
2026-06-17 00:55:06 +0200

    feat: finish js email list exercise, excluding bonus and style

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,21 @@ EXPECTED RESPONSE (random email address):
 // TODO Generate 10 random email addresses and print them in the page inside a list
 
 const API_URL = 'https://flynn.boolean.careers/exercises/api/random/mail';
-axios.get(API_URL).then((response) => console.log(response.data));
+const MAX_EMAILS = 10;
+
+const emailList = [];
+
+for (let i = 0; i < MAX_EMAILS; i++) {
+  axios
+    .get(API_URL)
+    .then((response) => {
+      const { success, response: email } = response.data;
+
+      if (success) emailList.push(email);
+
+      if (emailList.length === MAX_EMAILS || i >= MAX_EMAILS - 1) {
+        console.log(emailList);
+      }
+    })
+    .catch((error) => console.error(`Request failed: ${error.message}`));
+}
