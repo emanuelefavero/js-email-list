eaa78c65b8bb999d97b1e3a316e9d2ea3921670b
Emanuele Favero <emafaux89@gmail.com>
2026-06-17 00:34:16 +0200

    test: add .http test endpoint for random email address

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-console.log('js works');
+/*
+URL: https://flynn.boolean.careers/exercises/api/random/mail
+EXPECTED RESPONSE (random email address):
+{
+  "success": true,
+  "response": "bogan.zakary@hotmail.com"
+}
+*/
+
+// TODO Generate 10 random email addresses and print them in the page inside a list
