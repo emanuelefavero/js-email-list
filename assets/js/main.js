7c30f756523683a5ac99d543569f10a62af3cd41
Emanuele Favero <emafaux89@gmail.com>
2026-06-17 00:41:50 +0200

    feat: add first response check

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,6 @@ EXPECTED RESPONSE (random email address):
 */
 
 // TODO Generate 10 random email addresses and print them in the page inside a list
+
+const API_URL = 'https://flynn.boolean.careers/exercises/api/random/mail';
+axios.get(API_URL).then((response) => console.log(response.data));
