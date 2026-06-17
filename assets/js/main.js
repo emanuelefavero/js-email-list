c5888bdff6ac407dd3356b53003c23dd682b0a53
Emanuele Favero <emafaux89@gmail.com>
2026-06-17 03:32:37 +0200

    feat: add reload button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 // DOM ELEMENTS
 const output = document.getElementById('output');
+const reloadButton = document.getElementById('reload-button');
 
 // CONSTANTS
 const API_URL = 'https://flynn.boolean.careers/exercises/api/random/mail';
@@ -15,17 +16,14 @@ const getRandomEmail = () => {
 };
 
 const getRandomEmails = (total) => {
-  const requests = [];
-
-  for (let i = 0; i < total; i++) {
-    requests.push(getRandomEmail());
-  }
-
-  return Promise.all(requests);
+  const emailPromises = Array.from({ length: total }, () => getRandomEmail());
+  return Promise.all(emailPromises);
 };
 
 // TEMPLATES
 const getEmailTemplate = (email) => /*html*/ `<li>${email}</li>`;
+const getLoaderTemplate = () => /*html*/ `<li class="loader"></li>`;
+const getErrorTemplate = () => /*html*/ `<li>Unable to load emails</li>`;
 
 const getEmailListTemplate = (emails) => {
   return emails.map(getEmailTemplate).join('');
@@ -36,12 +34,34 @@ const renderEmailList = (element, emails) => {
   element.innerHTML = getEmailListTemplate(emails);
 };
 
+const renderLoader = (element) => {
+  element.innerHTML = getLoaderTemplate();
+};
+
+const renderError = (element) => {
+  element.innerHTML = getErrorTemplate();
+};
+
+const showReloadButton = () => reloadButton.classList.remove('hidden');
+const hideReloadButton = () => reloadButton.classList.add('hidden');
+
+// APP (Controller)
+const loadEmails = () => {
+  renderLoader(output);
+  hideReloadButton();
+
+  getRandomEmails(MAX_EMAILS)
+    .then((emails) => {
+      renderEmailList(output, emails);
+      showReloadButton();
+    })
+    .catch((error) => {
+      console.error(error);
+      renderError(output);
+      showReloadButton();
+    });
+};
+
 // INIT
-getRandomEmails(MAX_EMAILS)
-  .then((emails) => {
-    renderEmailList(output, emails);
-  })
-  .catch((error) => {
-    console.error(error);
-    output.innerHTML = /*html*/ `<li>Unable to load emails</li>`;
-  });
+loadEmails();
+reloadButton.addEventListener('click', loadEmails);
